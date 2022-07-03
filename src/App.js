c48d1680ef9fc284972ef87bdee0fef71b50d294
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-03 23:16:29 +0300

    Complete main additions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import SidebarItem from './components/SidebarItem'
+import Slider from './components/Slider'
+import { useState } from 'react'
+
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Saturation',
+    property: 'saturate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: '%',
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: 'deg',
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 10,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: 'px',
+  },
+]
 
 function App() {
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+
+  const selectedOption = options[selectedOptionIndex]
+
+  const handleSliderChange = ({ target }) => {
+    setOptions((prevOptions) => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedOptionIndex) return option
+        return { ...option, value: target.value }
+      })
+    })
+    getImageStyle()
+  }
+
+  const getImageStyle = () => {
+    const filters = options.map((option) => {
+      return `${option.property}(${option.value}${option.unit})`
+    })
+    return { filter: filters.join(' ') }
+  }
+  console.log(getImageStyle())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='main-image' style={getImageStyle()} />
+      <div className='sidebar'>
+        {options.map((option, index) => {
+          return (
+            <SidebarItem
+              key={index}
+              name={option.name}
+              active={index === selectedOptionIndex}
+              handleClick={() => {
+                setSelectedOptionIndex(index)
+              }}
+            />
+          )
+        })}
+      </div>
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleSliderChange}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
